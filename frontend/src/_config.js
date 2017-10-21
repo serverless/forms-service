6096f0bf591caacfbba2b2be1e8c80e741aa4f10
davidwells <david@inboundnow.com>
2017-10-21 16:40:23 -0700

    update prod URL

--- frontend/src/_config.js
+++ frontend/src/_config.js
@@ -2,13 +2,13 @@ const isDev = (process.env.NODE_ENV === 'development')
 
 export const config = {
   // cors settings
-  domain: (isDev) ? 'http://localhost:3000' : 'http://sls-forms.surge.sh',
+  domain: (isDev) ? 'http://localhost:3000' : 'https://serverless-forms.netlify.com/',
   // auth0 setup
   auth0: {
     domain: 'serverlessdev.auth0.com',
     clientId: 'Rj8Xz7lD9k01M046leIqk8CdmAwCCYbm',
     apiUrl: 'throwaway',
-    callbackUrl: (isDev) ? 'http://localhost:3000/callback' : 'http://sls-forms.surge.sh/callback'
+    callbackUrl: (isDev) ? 'http://localhost:3000/callback' : 'https://serverless-forms.netlify.com/callback'
   },
   // api endpoints
   api: {
