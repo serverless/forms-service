43a7d24d77a171ca0e64fd7ce3740cd54053f8d5
davidwells <david@inboundnow.com>
2017-10-20 19:51:13 -0700

    update api endpoints

--- frontend/src/_config.js
+++ frontend/src/_config.js
@@ -1,6 +1,18 @@
+const isDev = (process.env.NODE_ENV === 'development')
+
 export const config = {
+  // cors settings
+  domain: (isDev) ? 'http://localhost:3000' : 'http://sls-forms.surge.sh',
+  // auth0 setup
+  auth0: {
+    domain: 'serverlessdev.auth0.com',
+    clientId: 'Rj8Xz7lD9k01M046leIqk8CdmAwCCYbm',
+    apiUrl: 'throwaway',
+    callbackUrl: (isDev) ? 'http://localhost:3000/callback' : 'http://sls-forms.surge.sh/callback'
+  },
+  // api endpoints
   api: {
     forms: 'https://d3ul21vxig.execute-api.us-west-2.amazonaws.com/prod/get-forms',
-    submissions: 'https://d3ul21vxig.execute-api.us-west-2.amazonaws.com/prod/get-submissions'
+    submissions: 'https://d3ul21vxig.execute-api.us-west-2.amazonaws.com/prod/get-entries'
   }
 }
