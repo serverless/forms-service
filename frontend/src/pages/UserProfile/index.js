e8a5e46f66d23775c2732434e0105f7d7697907a
davidwells <david@inboundnow.com>
2018-01-11 14:11:54 -0800

    update profile view

--- frontend/src/pages/UserProfile/index.js
+++ frontend/src/pages/UserProfile/index.js
@@ -10,15 +10,27 @@ export default class Profile extends Component {
       return <div>Loading</div>
     }
 
+    const image = (profile.picture) ? <img src={profile.picture} alt="profile" /> : null
+
+    let nickname
+    if (profile.picture) {
+      nickname = (
+        <div>
+          Nickname
+          <h3>{profile.nickname}</h3>
+        </div>
+      )
+    }
+
     return (
       <AppLayout>
         <h1>{profile.name}</h1>
         <div>
-          <img src={profile.picture} alt="profile" />
-          <div>
-            Nickname
-            <h3>{profile.nickname}</h3>
-          </div>
+          {image}
+          {nickname}
+        </div>
+        <div>
+          <h2>User data from JWT</h2>
           <pre>
             {JSON.stringify(profile, null, 2)}
           </pre>
